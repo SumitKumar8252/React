335e36a5eb31dea156ceb8934650ed9b8ab5ef78
SumitKumar8252 <sk497623@gmail.com>
2025-09-01 20:28:24 +0530

    commit

---
+++ Backend/helper.js
@@ -0,0 +1,10 @@
+// ESM MODULE SYSTEM
+
+export function fun(){
+console.log("Hellow from Helper")
+}
+
+export function sum(a,b){
+    return `Sum of two number is ${a+b}`
+}
+
---
+++ Backend/index.js
@@ -0,0 +1,6 @@
+import { fun, sum } from "./helper.js";
+
+fun();
+console.log(sum(2, 3));
+
+console.log("Hello from Node");
