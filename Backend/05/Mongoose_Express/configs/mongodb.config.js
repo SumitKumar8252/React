89c28e4256439e15f7b1c23d7698d3d1677c53f5
SumitKumar8252 <sk497623@gmail.com>
2025-09-26 17:49:18 +0530

    commit

--- Backend/05/Mongoose_Express/configs/mongodb.config.js
+++ Backend/05/Mongoose_Express/configs/mongodb.config.js
@@ -1,1 +1,14 @@
 // we will create a function with conecting mongoDB and NodeJS
+
+const mongoose = require("mongoose");
+
+const connectToDB = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/mongooseTest");
+    console.log("Conneted to DB..");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports= connectToDB;
--- Backend/05/Mongoose_Express/server.js
+++ Backend/05/Mongoose_Express/server.js
@@ -1,5 +1,14 @@
 require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
 
+const express = require("express");
+const connectToDB = require("./configs/mongodb.config");
+
+connectToDB()
 const app = express();
+
+app.use(express.json())
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log("Server is Ready !!")
+})
