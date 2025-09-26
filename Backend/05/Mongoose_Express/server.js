3988711d4f5acff466be89ea0eff2fe910211b00
SumitKumar8252 <sk497623@gmail.com>
2025-09-26 18:09:59 +0530

    commit

--- Backend/05/Mongoose_Express/server.js
+++ Backend/05/Mongoose_Express/server.js
@@ -8,7 +8,9 @@ const app = express();
 
 app.use(express.json())
 
-
+app.get("/test", (req, res)=>{
+    res.send("<h1> CONNECTED </h1>")
+})
 app.listen(process.env.PORT, ()=>{
     console.log("Server is Ready !!")
 })
